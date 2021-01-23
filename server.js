c20fffcecb73ceb8100a17451a6d2108053df141
krishna kesarwani <krishnak1499@gmail.com>
2021-01-23 17:42:42 +0530

    initial bootcamp controller and bootcamp model created

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 const express=require('express');
 const dotenv=require('dotenv');
+const morgan=require('morgan');
+const colors=require('colors');
+const connectDB=require('./config/db');
 
 //Load environment variables
 dotenv.config({path:'./config/config.env'});
 
+//connect database
+connectDB();
+
+//Route files
+const bootcamps=require('./routes/bootcamps');
+
 const app=express();
+
+//Body Parser
+app.use(express.json());
+
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+//Mount routers
+app.use('/api/v1/bootcamps',bootcamps);
 
 const Port=5000 || process.env.PORT;
 
-app.listen(Port,()=> console.log(`The server is running in ${process.env.NODE_ENV} mode on port: ${Port}`));
+
+const server=app.listen(Port,()=> console.log(`The server is running in ${process.env.NODE_ENV} mode on port: ${Port}`.yellow.bold));
+
+//Handle unhandled promise rejections.
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`error: ${err.message}`.red);
+    //close server and exit process
+    server.close(()=>process.exit(1));
+});
