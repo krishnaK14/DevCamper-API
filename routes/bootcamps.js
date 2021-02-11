83066f40c2d27e25d4a1158f79a3fb28f2a6adb2
krishna kesarwani <krishnak1499@gmail.com>
2021-02-11 21:43:29 +0530

    added reviews and rating model and functionality to the api

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,6 +5,7 @@ const Bootcamp=require('../models/Bootcamp');
 
 //Include other resource routers
 const courseRouter=require('./courses');
+const reviewRouter=require('./reviews');
 
 const router=express.Router();
 
@@ -12,7 +13,10 @@ const advancedResults=require('../middleware/advancedResults');
 const {protect,authorize}=require('../middleware/auth');
 
 //Re-route into other resource routers
-router.use('/:bootcampId/courses',courseRouter)
+router.use('/:bootcampId/courses',courseRouter);
+router.use('/:bootcampId/reviews',reviewRouter);
+
+
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/:id/photo').put(protect,authorize('publisher','admin'),bootcampPhotoUpload);
 router.route('/').get(advancedResults(Bootcamp,'courses'),getBootcamps).post(protect,authorize('publisher','admin'),createBootcamp);
